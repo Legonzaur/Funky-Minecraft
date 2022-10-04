6a0a3ad798b30956ee884852a5804b6d526eaa1d
Legonzaur <legonzaur@live.fr>
2022-10-05 00:08:35 +0200

    nerf ProjectE

--- kubejs/server_scripts/recipes/projecte.js
+++ kubejs/server_scripts/recipes/projecte.js
@@ -1,4 +1,6 @@
 onEvent("recipes", (event) => {
     event.replaceInput("projecte:watch_of_flowing_time", "projecte:archangel_smite")
     event.remove({id:"projecte:watch_of_flowing_time"})
+    event.replaceInput({output:"projecte:philosophers_stone"}, "minecraft:diamond", "extendedcrafting:the_ultimate_ingot")
+    event.replaceInput({mod:"projecte"}, "minecraft:diamond_block", "extendedcrafting:the_ultimate_block")
 })
