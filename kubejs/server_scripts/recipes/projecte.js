cb846900a59e79a273afca9db074f480ae08618e
Legonzaur <legonzaur@live.fr>
2022-10-05 00:48:34 +0200

    endgame transmutation table gating

--- kubejs/server_scripts/recipes/projecte.js
+++ kubejs/server_scripts/recipes/projecte.js
@@ -3,4 +3,5 @@ onEvent("recipes", (event) => {
     event.remove({id:"projecte:watch_of_flowing_time"})
     event.replaceInput({output:"projecte:philosophers_stone"}, "minecraft:diamond", "extendedcrafting:the_ultimate_ingot")
     event.replaceInput({mod:"projecte"}, "minecraft:diamond_block", "extendedcrafting:the_ultimate_block")
+    event.replaceInput({output:"projecte:transmutation_table"}, "projecte:philosophers_stone", "avaritia:infinity_catalyst")
 })
